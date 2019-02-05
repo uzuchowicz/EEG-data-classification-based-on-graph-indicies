f56c7a504b9486acbae5dd2bb19425f389cd75fc
uzuchowicz <uzuchowicz@gmail.com>
2019-02-05 22:22:42 +0100

    knn-classification v0.2

--- scripts/knn_classifier.py
+++ scripts/knn_classifier.py
@@ -4,6 +4,7 @@ from sklearn import metrics
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Classifier:
     def __init__(self):
@@ -14,31 +15,43 @@ class Classifier:
         model = KNeighborsClassifier(n_neighbors)
 
         model.fit(data_train, labels)
+        data_test = np.array(data_test).reshape(1,1)
 
         predicted = model.predict(data_test)
-        accuracy = metrics.accuracy_score(data_train, data_test)
-        return predicted, accuracy
+        return predicted
+
+    def test_param(self,data, parameters, group_factor):
+        self.data = data
+        if not isinstance(parameters, list):
+            parameters = [parameters]
+        data_param = data.filter(parameters, axis=1)
+        k_list = [i for i in range(1,2)]
+        group_labels = self.data[group_factor]
+        result_data = pd.DataFrame(columns={'n_neighbors', 'predicted', 'label', 'result', 'item'})
+        k_accuracy = pd.DataFrame(columns={'n_neighbors', 'accuracy'})
+        for n_neighbors in k_list:
+            for counter, label in enumerate(group_labels):
+                #X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+                data_test = data_param.iloc[counter,:]
+                label = group_labels.iloc[counter]
+                data_train_labels=group_labels.drop(axis=0, index=counter)
+                data_train = data_param.drop(axis=0, index=counter)
+
+                predicted = Classifier.sklearn_classification(data_train, data_test, data_train_labels, n_neighbors)
+                result = 0
+                if predicted == label:
+                    result = 1
+                result_data.append({'n_neighbors': n_neighbors, 'predicted': predicted, 'label': label, 'result': result,
+                                    'item': counter}, ignore_index=True)
+            k_accuracy.append({'n_neighbors': n_neighbors, 'accuracy': metrics.accuracy_score(result_data['predicted'],
+                                                                                            result_data['label'])}, ignore_index=True)
+        return result_data, k_accuracy
+
 
     @staticmethod
-    def test_param(data, parameter):
-
-        k_list = [i for i in range(15)]
-        group_labels = data['group']
-        result_data = pd.DataFrame(columns={'preditcted', 'accuracy', 'n_neighbors', 'label','item' })
-
-        for counter, label in enumerate(group_labels):
-            #X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
-            data_train = data.drop(axis=0, index = counter)
-            data_test = data.iloc[counter,:]
-            for n_neighbors in k_list:
-                predicted, accuracy = Classifier.sklearn_classification(data_train, data_test, group_labels, n_neighbors)
-                result_data.append({'preditcted':predicted, 'accuracy':accuracy, 'n_neighbors':n_neighbors, 'label':label,'item':counter })
-        return result_data
-
-
-    @staticmethod
-    def result_visualization(result_data, parameters):
-        ax = sns.lineplot(x="n_neighbors", y="accuracy", hue = "event", style = "event", markers = True, dashes = False, data = result_data)
+    def result_visualization(data, result_data, k_accuracy, parameters):
+        ax = sns.lineplot(x="n_neighbors", y="result", hue = "event", style = "event", markers = True, dashes = False, data = result_data)
         plt.show()
-        # sns.lineplot(x="n_neighbors", y="predicted", hue="event", style="event", markers=True, dashes=False,
-        #              data=result_data)
+        sns.lineplot()
+        sns.lineplot(x="n_neighbors", y="predicted", hue="event", style="event", markers=True, dashes=False,
+                     data=result_data)
